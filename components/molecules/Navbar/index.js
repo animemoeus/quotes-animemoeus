d5114cbe60295f4dcbf933b9dc18e9d4c8391ecb
Arter Tendean <arter.tendean.07@gmail.com>
2021-05-30 19:57:02 +0800

    adding navbar

---
+++ components/molecules/Navbar/index.js
@@ -0,0 +1,31 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg shadow-sm sticky-top navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import { Navbar } from "../components/molecules";
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
+
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
         distinctio explicabo, harum, vero eius saepe totam reprehenderit ad
